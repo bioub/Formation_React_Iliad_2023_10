7a5ed296beebc6217a25f71565009a5c94cbab08
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-23 14:44:38 +0200

    JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const now = new Date();
+  const prenom = 'Romain';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="App">
+      <p>Heure : {now.toLocaleTimeString()}</p>
+      <p>Prénom : {prenom}</p>
     </div>
+
+    // Sans JSX avec l'API React.createElement (legacy)
+    // React.createElement('div', { className: 'App', id: 'App' },
+    //   React.createElement('p', null, 'Heure : ', now.toLocaleTimeString()),
+    //   React.createElement('p', null, 'Prénom : ', prenom)
+    // );
   );
 }
 
 export default App;
+
+// En JSX
+// Lorsqu'on utilise des accolades { }, on demande à afficher le résultat
+// d'une expression, ex: {1 + 1} affichera 2 {"Romain".toUpperCase()} affichera ROMAIN
+// si le type est string ou number le résultat s'affiche
+// si le type est null, undefined ou boolean rien ne s'affiche
+// si le type est string[], les éléments s'affiche (1 noeud de Text par élément)
+// si le type est objet === erreur (sauf array et string qui sont des object, où React.Element)
+// Objects are not valid as a React child
+
+// Au niveau d'un composant :
+// - on ne peut retourner qu'une seule valeur (techniquement n'importe expression comme ci-dessus)
+// - si on retourne du JSX (donc un React.Element), il faut une balise qui englobe les autres (ici <div>)
+// car on ne peut retourner qu'une seule valeur dans une fonction (éventuellement un Fragment)
+// - par convention, souvent la balise racine du composant a comme classe le nom du composant
+// - obligation : le composant commence par une majuscule
+// - de préférence (lié à React.lazy) le composant est exporté par défaut (export default App)
+// - après le return il est souvent nécessaire d'utiliser des parenthèses ()
